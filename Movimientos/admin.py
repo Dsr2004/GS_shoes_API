a124ef5b35cf31e24911a4e63c1035390385596b
JuanMaGaviria <juan.gaviria@lambdananalytics.co>
2025-06-08 18:36:46 -0500

    feat(movimientos): Creación de aplicación movimientos

---
+++ Movimientos/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import MovimientoInventario, SalidaInventario
+
+
+admin.site.register(MovimientoInventario)
+admin.site.register(SalidaInventario)
+
+# Register your models here.
